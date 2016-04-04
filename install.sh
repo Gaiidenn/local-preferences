6bbb7fb197dd038a9e4d0cbeb8a3aec4af7143f6
Jerome <jerome@gaatech.be>
2016-04-04 08:52:07 +0200

    install.sh modified

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
-cp -rf ./.config/ ~/
-git clone https://github.com/amix/vimrc.git ~/.vim_runtime
+cp -rf .config/ ~/.config/
+git clone https://github.com/amix/vimrc.git ~/.vim_runtime/
+git clone https://github.com/leafgarland/typescript-vim.git ~/.vim_runtime/sources_non_forked/typescript-vim
 sh ~/.vim_runtime/install_awesome_vimrc.sh
